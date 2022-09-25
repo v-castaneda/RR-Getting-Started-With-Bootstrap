be470444e0d6e43a35511e94c04d888745cff3d7
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-25 11:12:38 -0400

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-
-
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
 
 function App() {
   return (
     <div className="App">
-      <h1>Bootstrap Website</h1>
-     
+      <Nav activeKey='/home'>
+        <Nav.Item>
+          <Nav.Link href='/home'>Home</Nav.Link>
+        </Nav.Item>
+      </Nav>
+
+      <h1>Hello World</h1>
+      <Button variant='danger'>Danger</Button>
     </div>
   );
 }
